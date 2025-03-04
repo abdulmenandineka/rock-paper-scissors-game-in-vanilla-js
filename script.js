f1c5e45e918179a1da102b4f66f1323a1e5408df
abdulmenandineka <abdulmenandineka6@gmail.com>
2025-03-04 12:12:37 +0300

    adding parameter & css
    for  reducing code line, around above 40 line code was removed. now we will get computerMove and playerMove dynamically. otherwise  amazing style view css has been added
    
    Co-Authored-By: abdulmenandineka <173333076+abdulmenandineka@users.noreply.github.com>

--- script.js
+++ script.js
@@ -17,76 +17,37 @@ let rockBtn = document.querySelector('.rock'),
     scoreP = document.querySelector('.score');
 
 
-rockFunction =()=>{
-    let compMove = computerMove(),
+    playGame = (playerMOve) =>{
+        let compMove = computerMove(),
         result = '';
-        compMove === 'rock' ? result = 'tie' : 
-        compMove === 'paper' ? result = 'you lose':
-        compMove === 'scissors' ? result = 'you win' : '';
-    let moveHtml = `you pick <span style = 'color:red;font-size:30px'>rock</span> computer pick <span style = 'color:red;font-size:30px'>${compMove}</span>`
+        playerMOve === compMove ? result = 'tie' :
+        
+        playerMOve === 'rock' && compMove === 'scissors' ||
+        playerMOve === 'paper' && compMove === 'rock' ||
+        playerMOve === 'scissors' && compMove === 'paper' ? result = 'you win' :
 
-    // console.log(`you pick rock computer pick ${compMove}`);
-    console.log(`result ${result}`);
+        playerMOve === 'rock' && compMove === 'paper' ||
+        playerMOve === 'paper' && compMove === 'scissors' ||
+        playerMOve === 'scissors' && compMove === 'rock' ? result = 'you lose' : '';
+        
+        // console.log(`you pick ${playerMOve} computer pick ${compMove}`);
+        // console.log(`result :  ${result}`);
 
-    result === 'you win' ? score.win++ :
-    result === 'tie' ? score.tie++ :
-    result === 'you lose' ? score.lose++ : ''; 
+        result === 'you win' ? score.win++ :
+            result === 'tie' ? score.tie++ :
+            result === 'you lose' ? score.lose++ : ''; 
 
-    move.innerHTML = moveHtml;
-    resultHtml.innerHTML = `result : <span style = 'color:blue;font-size:40px;'>${result}</span>`;
-    scoreP.textContent = `win:${score.win} tie:${score.tie} lose:${score.lose}`
-    console.log(`win:${score.win} tie:${score.tie} lose:${score.lose}`);
-    
-};
-
-
-paperFunction =()=>{
-    let compMove = computerMove(),
-        result = '';
-        compMove === 'rock' ? result = 'you win' : 
-        compMove === 'paper' ? result = 'tie':
-        compMove === 'scissors' ? result = 'you lose' : '',
-        moveHtml = `you pick <span style = 'color:red;font-size:30px'>paper</span> computer pick <span style = 'color:red;font-size:30px'>${compMove}</span>`;
-    // console.log(`you pick paper computer pick ${compMove}`);
-    console.log(`result ${result}`);
-
-    result === 'you win' ? score.win++ :
-    result === 'tie' ? score.tie++ :
-    result === 'you lose' ? score.lose++ : ''; 
-
-
-    move.innerHTML = moveHtml;
-    resultHtml.innerHTML = `result : <span style = 'color:blue;font-size:40px;'>${result}</span>`;
-    scoreP.textContent = `win:${score.win} tie:${score.tie} lose:${score.lose}`
-    console.log(`win:${score.win} tie:${score.tie} lose:${score.lose}`);
-}
+        let moveHtml = `you pick <span style = 'color:red;font-size:30px'>${playerMOve}</span> computer pick <span style = 'color:red;font-size:30px'>${compMove}</span>`
+            move.innerHTML = moveHtml;
+            resultHtml.innerHTML = `result : <span style = 'color:blue;font-size:40px;'>${result}</span>`;
+            scoreP.textContent = `win:${score.win} tie:${score.tie} lose:${score.lose}`
+            console.log(`win:${score.win} tie:${score.tie} lose:${score.lose}`);
+    }
 
 
 
-scissorsFunction =()=>{
-    let compMove = computerMove(),
-        result = '';
-        compMove === 'rock' ? result = 'you lose' : 
-        compMove === 'paper' ? result = 'you win':
-        compMove === 'scissors' ? result = 'tie' : '',
-        moveHtml = `you pick <span style = 'color:red;font-size:30px'>scissors</span> computer pick <span style = 'color:red;font-size:30px'>${compMove}</span>`
-    // console.log(`you pick scissors computer pick ${compMove}`);
-    console.log(`result ${result}`);
-
-    result === 'you win' ? score.win++ :
-    result === 'tie' ? score.tie++ :
-    result === 'you lose' ? score.lose++ : ''; 
 
 
-    move.innerHTML = moveHtml;
-    resultHtml.innerHTML = `result : <span style = 'color:blue;font-size:40px;'>${result}</span>`;
-    scoreP.textContent = `win:${score.win} tie:${score.tie} lose:${score.lose}`
-    console.log(`win:${score.win} tie:${score.tie} lose:${score.lose}`);
-}
 
-computerMove=()=>{
-    let randomNumber = Math.floor(Math.random() * 3 + 1 ),
-    computerMove = '';
-    return randomNumber == 1 ? computerMove = 'rock': randomNumber == 2 ? computerMove = 'paper': randomNumber == 3 ? computerMove = 'scissors' : '';
 
-}
+        
